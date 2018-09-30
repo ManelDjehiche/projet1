b716501bc9f0fc775015dbd728e51c01ce76e490
Manel dje <maneldje81@gmail.com>
2018-09-30 15:35:14 +0200

    modfier page d'acceuil

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -71,9 +71,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Bonjours tous le monde c manel</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p>this is my my </p> 
 		<code>application/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
