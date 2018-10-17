43b964386da53c263d9c940dc44e2dbe91a3168a
KaousHatem <kaous.nadir.hatem@gmail.com>
2018-10-17 22:44:36 +0100

    fix base url

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,32 +13,32 @@
     CSS
     ==================================================
     -->
-    <link href="assets/template/fonts/fonts.css" rel="stylesheet">
-    <link href="assets/template/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/template/vendor/elegant-icons/style.css" rel="stylesheet">
-    <link href="assets/template/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/template/vendor/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/template/vendor/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="assets/template/vendor/css-hamburgers/dist/hamburgers.min.css" rel="stylesheet">
-    <link href="assets/template/vendor/chosen/chosen.css" rel="stylesheet">
-    <link href="assets/template/vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="assets/template/vendor/plyr/plyr.min.css" rel="stylesheet">
-    <link href="assets/template/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
-    <link href="assets/template/vendor/revolution/css/layers.css" rel="stylesheet">
-    <link href="assets/template/vendor/revolution/css/navigation.css" rel="stylesheet">
-    <link href="assets/template/vendor/revolution/css/settings.css" rel="stylesheet">
-    <link href="assets/template/css/main.css" rel="stylesheet">
-    <link href="assets/template/css/switcher.css" rel="stylesheet">
-    <link href="assets/template/css/colors/primary.css" rel="stylesheet" id="colors">
-    <link href="assets/template/css/retina.css" rel="stylesheet">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/fonts/fonts.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/elegant-icons/style.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/css-hamburgers/dist/hamburgers.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/chosen/chosen.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/plyr/plyr.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/revolution/css/layers.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/revolution/css/navigation.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/vendor/revolution/css/settings.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/css/main.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/css/switcher.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/css/colors/primary.css" rel="stylesheet" id="colors">
+    <link href="<?php echo base_url()?>assets/template/css/retina.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     
     <!--
     portfolio
     ==================================================
     -->
-    <link href="assets/template/css/3-col-portfolio.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/template/css/3-col-portfolio.css" rel="stylesheet">
 
     <!--
     data table
@@ -54,10 +54,10 @@
     Favicons
     ==================================================
     -->
-    <link rel="shortcut icon" href="assets/template/favicon.png">
-    <link rel="apple-touch-icon" href="assets/template/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/template/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="assets/template/apple-touch-icon-114x114.png">
-    <script src="assets/template/js/modernizr-custom.js"></script>
+    <link rel="shortcut icon" href="<?php echo base_url()?>assets/template/favicon.png">
+    <link rel="apple-touch-icon" href="<?php echo base_url()?>assets/template/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url()?>assets/template/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url()?>assets/template/apple-touch-icon-114x114.png">
+    <script src="<?php echo base_url()?>assets/template/js/modernizr-custom.js"></script>
 </head>
 
